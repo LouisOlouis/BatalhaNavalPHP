e7cbb6087b9862db4c8b0730c5699b567100935a
LouisOlouis <Lcoliveiraaguiar@gmail.com>
2025-11-19 12:19:26 -0300

    Add game entry form and host functionality

---
+++ host.php
@@ -0,0 +1,34 @@
+<?php 
+session_start();
+
+//cria servidor
+$dirname = "servidor/";
+if (!is_dir($dirname)) {
+    mkdir($dirname, 0777, true);
+}
+$serverabertos = glob("$dirname{*.php}", GLOB_BRACE);
+$id = count($serverabertos) + 1;
+$servername = $_SERVER['DOCUMENT_ROOT'] . '/' . $dirname . "servidor_" . $id . ".php";
+
+if (!isset($_SESSION['serverc'])) {
+   $_SESSION['serverc'] = $servername;
+} else {
+    $servername = $_SESSION['serverc'];
+}
+
+$fp = fopen($servername,'w');
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Host - BatalhaNavalPHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>Você está hospedando um jogo!</h1>
+    <h3>Jogadores:</h3>
+    <h4><?php echo $_SESSION['usuario']; ?></h4>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-<?php ?>
+<?php 
+session_start();
+
+$mensagem = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['player_name'])) {
+        $_SESSION['usuario'] = $_POST['player_name'];
+        if (isset($_POST['host'])) {
+            header('Location: host.php');
+            exit;
+        } elseif (isset($_POST['join'])) {
+            if (isset($_POST['game_id'])) {
+                $game_id = $_POST['game_id'];
+                header('Location: join.php?id=' . $game_id);
+                exit;
+            }
+        }
+    } else {
+        $mensagem = 'Insira um nome de usuario';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>BatalhaNavalPHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>Batalha Naval em PHP</h1>
+    <h3>Feito por louis_louis</h3>
+    <form method="post">
+        <h3>Entre:</h3>
+        <input type="text" name="player_name" placeholder="Seu nome" required>
+        <br><br>
+        <button type="submit" name="host">Entrar como host</button>
+        <br><br>
+        <input type="number" name="game_id" placeholder="ID do jogo">
+        <button type="submit" name="join">Entrar como jogador</button>
+    </form>
+    <h4><?php echo $mensagem; ?></h4>
+</body>
+</html>
